b91a8d61466a2d2fc0d96e3d4a006096c809ff9c
ggiacz <gabrielgiacomopaes@gmail.com>
2023-05-30 19:46:48 -0300

    add state module and imports

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+import { estado } from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia)
 
-let estado = 0;
+
+
+atualiza();
 
 function envia(evento){
     evento.preventDefault()
