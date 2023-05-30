fe18d0da7e151f611add3ee934897dfd3ffea226
ggiacz <gabrielgiacomopaes@gmail.com>
2023-05-30 19:56:58 -0300

    add default exportation

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { estado } from "./store.js";
+import store from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia)
@@ -10,13 +10,13 @@ atualiza();
 function envia(evento){
     evento.preventDefault()
     console.log('Formulário enviado!')
-    estado++;
+    store.estado++;
     atualiza();
 }
 
 function atualiza()
 {
     const ol = document.querySelector('ol')
-    ol.innerHTML = `<li>${estado}<li/>`;   
+    ol.innerHTML = `<li>${store.estado}<li/>`;   
 }
 
